4aa125d5c06741e9a57dbbb665335a359ec1e318
simon-poon <93788837+simon-poon@users.noreply.github.com>
2023-04-08 18:31:37 -0400

    Update models.py

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ import numpy as np
 import itertools
 
 
+
 def dense_embedding(n_features=6,
                     n_features_cat=2,
                     activation='relu',
@@ -93,7 +94,6 @@ def dense_embedding_quantized(n_features=6,
 
     inputs_cont = Input(shape=(number_of_pupcandis, n_features-2), name='input_cont')
     pxpy = Input(shape=(number_of_pupcandis, 2), name='input_pxpy')
-
     embeddings = []
     inputs = [inputs_cont, pxpy]
     for i_emb in range(n_features_cat):
@@ -176,7 +176,57 @@ class weighted_sum_layer(Layer):
     def call(self, inputs):
         return tf.reduce_sum(inputs, axis=1)
 
-def graph_embedding(compute_ef, n_features=6,
+class ConcreteSelect(Layer):
+
+
+    def __init__(self, output_dim, start_temp = 10.0, min_temp = 0.1, alpha = 0.99999, **kwargs):
+        self.output_dim = output_dim
+        self.start_temp = start_temp
+        self.min_temp = K.constant(min_temp)
+        self.alpha = K.constant(alpha)
+        super(ConcreteSelect, self).__init__(**kwargs)
+    
+
+    def build(self, input_shape):
+        self.temp = self.add_weight(name = 'temp', shape = [], initializer = initializers.Constant(self.start_temp), trainable = False)
+        shape = (self.output_dim, input_shape[1])
+        self.logits = self.add_weight(name = 'logits', shape = (self.output_dim, input_shape[2]), initializer = initializers.glorot_normal(), trainable = True)      # (:, selected features, all features)
+        super(ConcreteSelect, self).build(input_shape)
+    
+
+    def call(self, X, training = None):
+        uniform = K.random_uniform(self.logits.shape, K.epsilon(), 1.0)
+        gumbel = -K.log(-K.log(uniform))
+        temp = K.update(self.temp, K.maximum(self.min_temp, self.temp * self.alpha))
+        noisy_logits = (self.logits + gumbel) / temp
+        samples = K.softmax(noisy_logits)
+    
+
+        discrete_logits = K.one_hot(K.argmax(self.logits), self.logits.shape[1])
+    
+
+        self.selections = K.in_train_phase(samples, discrete_logits, training)
+        print("X shape:  ", X.shape)
+        print("transpose self.selections shape", K.transpose(self.selections))
+        Y = K.dot(X, K.transpose(self.selections))
+    
+
+        return Y
+    
+    def get_config(self):
+        cfg = super(ConcreteSelect, self).get_config()
+        cfg['output_dim'] = self.output_dim
+        cfg['start_temp'] = self.start_temp
+        cfg['min_temp'] = self.min_temp
+        cfg['alpha'] = self.alpha
+        return cfg
+
+def graph_embedding(compute_ef, num_epochs, output_dim,
+                    batch_size=256,
+                    n_features=6,
+                    min_temp=0.01,
+                    start_temp=10.0,
+                    alpha=0.9999,
                     n_features_cat=2,
                     activation='relu',
                     number_of_pupcandis=100,
@@ -184,6 +234,7 @@ def graph_embedding(compute_ef, n_features=6,
                     emb_out_dim=8,
                     units=[64, 32, 16],
                     edge_list=[]):
+
     n_dense_layers = len(units)
     name = 'met'
 
@@ -208,7 +259,7 @@ def graph_embedding(compute_ef, n_features=6,
     Nr = N*(N-1)
     if compute_ef == 1:
         num_of_edge_feat = len(edge_list)
-        edge_feat = Input(shape=(Nr, num_of_edge_feat), name='edge_feat')
+        edge_feat = Input(shaape=(Nr, num_of_edge_feat), name='edge_feat')
         inputs.append(edge_feat)
 
     # can concatenate all 3 if updated in hls4ml, for now; do it pairwise
@@ -219,6 +270,7 @@ def graph_embedding(compute_ef, n_features=6,
     N = number_of_pupcandis
     P = n_features+n_features_cat
     Nr = N*(N-1)  # number of relations (edges)
+
 
     x = BatchNormalization()(x)
 
@@ -233,6 +285,13 @@ def graph_embedding(compute_ef, n_features=6,
 
     # Edges MLP
     h = Permute((2, 1), input_shape=node_feat.shape[1:])(node_feat)
+    
+    steps_per_epoch = batch_size
+    alpha = np.exp(np.log(min_temp / start_temp) / (num_epochs * steps_per_epoch))   
+
+
+    h = ConcreteSelect(output_dim=output_dim, start_temp=start_temp, min_temp=min_temp, alpha=alpha, name = 'concrete_select')(h)
+
     edge_units = [64, 32, 16]
     n_edge_dense_layers = len(edge_units)
     if compute_ef == 1:
