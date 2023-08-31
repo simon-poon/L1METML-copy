1d186a2b7dd8cbb65aa7d0374e8370767b5b2ee6
simon-poon <93788837+simon-poon@users.noreply.github.com>
2023-08-30 22:44:30 -0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,8 +1,9 @@
+import tensorflow.keras.backend as K
+import tensorflow as tf
+import numpy as np
+
 def custom_loss(y_true, y_pred):
-    '''
-    cutmoized loss function to improve the recoil response,
-    by balancing the response above one and below one
-    '''
+
     import tensorflow.keras.backend as K
     import tensorflow as tf
 
@@ -11,19 +12,15 @@ def custom_loss(y_true, y_pred):
     px_pred = K.flatten(y_pred[:, 0])
     py_pred = K.flatten(y_pred[:, 1])
 
-    pt_truth = K.sqrt(px_truth*px_truth + py_truth*py_truth)
+    pt_truth = K.sqrt(px_truth*px_truth + py_truth*py_truth + 1e-12)
+    pt_pred = K.sqrt(px_pred*px_pred + py_pred*py_pred + 1e-12)
 
-    #px_truth1 = px_truth / pt_truth
-    #py_truth1 = py_truth / pt_truth
-
-    # using absolute response
-    # upar_pred = (px_truth1 * px_pred + py_truth1 * py_pred)/pt_truth
-    upar_pred = K.sqrt(px_pred * px_pred + py_pred * py_pred) - pt_truth
+    upar_pred = pt_pred - pt_truth
     pt_cut = pt_truth > 0.
+    pt_pred_filtered = tf.boolean_mask(pt_pred, pt_cut)
     upar_pred = tf.boolean_mask(upar_pred, pt_cut)
     pt_truth_filtered = tf.boolean_mask(pt_truth, pt_cut)
 
-    #filter_bin0 = pt_truth_filtered < 50.
     filter_bin0 = tf.logical_and(pt_truth_filtered > 50.,  pt_truth_filtered < 100.)
     filter_bin1 = tf.logical_and(pt_truth_filtered > 100., pt_truth_filtered < 200.)
     filter_bin2 = tf.logical_and(pt_truth_filtered > 200., pt_truth_filtered < 300.)
@@ -48,11 +45,40 @@ def custom_loss(y_true, y_pred):
     dev += tf.abs(tf.reduce_sum(upar_pred_pos_bin2) + tf.reduce_sum(upar_pred_neg_bin2))
     dev += tf.abs(tf.reduce_sum(upar_pred_pos_bin3) + tf.reduce_sum(upar_pred_neg_bin3))
     dev += tf.abs(tf.reduce_sum(upar_pred_pos_bin4) + tf.reduce_sum(upar_pred_neg_bin4))
-    #dev += tf.abs(tf.reduce_sum(upar_pred_pos_bin5) + tf.reduce_sum(upar_pred_neg_bin5))
+
     dev /= norm
 
-    loss = 0.5*K.mean((px_pred - px_truth)**2 + (py_pred - py_truth)**2)
+    def huber_loss(y_true, y_pred, delta=1.0):
+        error = y_true - y_pred
+        abs_error = tf.abs(error)
+        quadratic_region = 0.5 * tf.square(abs_error)
+        linear_region = delta * abs_error - 0.5 * tf.square(delta)
+        loss = tf.where(abs_error < delta, quadratic_region, linear_region)
+        return tf.reduce_mean(loss)
 
-    #loss += 200.*dev
-    loss += 5000.*dev
-    return loss
+    def quantile_loss(y_true, y_pred, tau):
+        error = y_true - y_pred
+        loss = tf.where(error > 0, tau * error, (tau - 1) * error)
+        return tf.reduce_mean(loss)
+
+    delta = 1.0  # Huber loss delta
+    tau_25 = 0.25  # 25% quantile
+    tau_75 = 0.75  # 75% quantile
+ 
+    def calculate_quantile(predictions, quantile):
+        sorted_predictions = tf.sort(predictions)
+        index = tf.cast(tf.shape(sorted_predictions)[0], tf.float32) * tf.cast(quantile, tf.float32)
+        index = tf.cast(index, tf.int64)
+        quantile_value = sorted_predictions[index]
+        return quantile_value
+
+    pt_pred_25 = calculate_quantile(pt_pred,tau_25)
+    pt_pred_75 = calculate_quantile(pt_pred,tau_75)
+    
+    huber_loss_value = huber_loss(pt_truth, pt_pred, delta)
+    quantile_loss_25 = quantile_loss(pt_truth, pt_pred_25, tau_25)
+    quantile_loss_75 = quantile_loss(pt_truth, pt_pred_75, tau_75)
+    
+    complete_loss_value = huber_loss_value + quantile_loss_25 + quantile_loss_75
+    complete_loss_value += 5000.*dev
+    return complete_loss_value
