7387eee578748393a0eb99e463de4327166eb321
simon-poon <93788837+simon-poon@users.noreply.github.com>
2023-09-17 02:47:05 -0400

    Update models.py

--- models.py
+++ models.py
@@ -117,7 +117,7 @@ def dense_embedding(n_features=6,
             pxpy = Add()([pxpy, b])
         w = Dense(2, name='met_weight', activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
         w = BatchNormalization(trainable=False, name='met_weight_minus_one', epsilon=False)(w)
-        x = quantile_multiply_layer()([w, pxpy])
+        x = quantile_multiply_layer()(w, pxpy)
     for i_dense in [32,16,1]:
         x_quant = Dense(i_dense, activation='linear', kernel_initializer='lecun_uniform')(x)
         x_quant = BatchNormalization(momentum=0.95)(x_quant)
