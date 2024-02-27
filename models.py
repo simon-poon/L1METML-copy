b22682939a7254552c205f496da0d88567b0f12a
simon-poon <93788837+simon-poon@users.noreply.github.com>
2024-02-27 02:00:20 -0800

    Update models.py

--- models.py
+++ models.py
@@ -76,7 +76,7 @@ def dense_embedding(n_features=6,
                     n_features_cat=2,
                     activation='relu',
                     number_of_pupcandis=100,
-                    embedding_input_dim={0: 13, 1: 3},
+                    #embedding_input_dim={0: 13, 1: 3},
                     emb_out_dim=8,
                     with_bias=True,
                     t_mode=0,
@@ -84,27 +84,27 @@ def dense_embedding(n_features=6,
                     num_of_bins=50):
     n_dense_layers = len(units)
 
-    inputs_cont = Input(shape=(number_of_pupcandis, n_features-2), name='input_cont')
-    pxpy = Input(shape=(number_of_pupcandis, 2), name='input_pxpy')
+    inputs_cont = Input(shape=(2, 5), name='input_cont')
+    #pxpy = Input(shape=(2), name='input_pxpy')
 
-    embeddings = []
-    inputs = [inputs_cont, pxpy]
-    for i_emb in range(n_features_cat):
-        input_cat = Input(shape=(number_of_pupcandis, ), name='input_cat{}'.format(i_emb))
-        inputs.append(input_cat)
-        embedding = Embedding(
-            input_dim=embedding_input_dim[i_emb],
-            output_dim=emb_out_dim,
-            embeddings_initializer=initializers.RandomNormal(
-                mean=0,
-                stddev=0.4/emb_out_dim),
-            name='embedding{}'.format(i_emb))(input_cat)
-        embeddings.append(embedding)
+    inputs = [inputs_cont]
+    #for i_emb in range(n_features_cat):
+    #    input_cat = Input(shape=(number_of_pupcandis, ), name='input_cat{}'.format(i_emb))
+    #    inputs.append(input_cat)
+    #    embedding = Embedding(
+    #        input_dim=embedding_input_dim[i_emb],
+    #        output_dim=emb_out_dim,
+    #        embeddings_initializer=initializers.RandomNormal(
+    #            mean=0,
+    #            stddev=0.4/emb_out_dim),
+    #        name='embedding{}'.format(i_emb))(input_cat)
+    #    embeddings.append(embedding)
 
     # can concatenate all 3 if updated in hls4ml, for now; do it pairwise
     # x = Concatenate()([inputs_cont] + embeddings)
-    emb_concat = Concatenate()(embeddings)
-    x = Concatenate()([inputs_cont, emb_concat])
+    #emb_concat = Concatenate()(embeddings)
+    #x = Concatenate()([inputs_cont, emb_concat])
+    x = inputs_cont
 
     for i_dense in range(n_dense_layers):
         x = Dense(units[i_dense], activation='linear', kernel_initializer='lecun_uniform')(x)
@@ -112,36 +112,26 @@ def dense_embedding(n_features=6,
         x = Activation(activation=activation)(x)
 
     if t_mode == 0:
-        x = GlobalAveragePooling1D(name='pool')(x)
-        x = Dense(2, name='output', activation='linear')(x)
+        raise ValueError("Current version does not support this mode")
+        #x = GlobalAveragePooling1D(name='pool')(x)
+        #x = Dense(2, name='output', activation='linear')(x)
 
     if t_mode == 1:
-        if with_bias:
-            b = Dense(2, name='met_bias', activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
-            pxpy = Add()([pxpy, b])
-        w = Dense(1, name='met_weight', activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
-        w = BatchNormalization(trainable=False, name='met_weight_minus_one', epsilon=False)(w)
-        x = Multiply()([w, pxpy])
-        x = weighted_sum_layer(name='weighted_sum_layer')(x)
-
-        m = add_axis()(x)
-
-        units_list = [32,32]
-        for i_dense in range(2):
-            m = Dense(units_list[i_dense], activation='linear', kernel_initializer='lecun_uniform')(m)
-            m = BatchNormalization(momentum=0.95)(m)
-            m = Activation(activation=activation)(m)
-        m = Dense(num_of_bins, activation='linear', kernel_initializer='lecun_uniform')(m)
-        w = BatchNormalization(momentum=0.95)(m)
+        #if with_bias:
+        #    b = Dense(num_of_bins, name='met_bias', activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
+        #    pxpy = Add()([pxpy, b])
+        x = Dense(num_of_bins, name='met_weight', activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
+        w = BatchNormalization(momentum=0.95)(x)
         w = Softmax(axis=-1)(w)
         m = bin_multiply(num_of_bins=num_of_bins)(w)
-
+        #x = BatchNormalization(trainable=False, name='met_weight_minus_one', epsilon=False)(w)
+        #x = Multiply()([w, pxpy])
 
     outputs = [m,w]
 
     keras_model = Model(inputs=inputs, outputs=outputs)
 
-    keras_model.get_layer('met_weight_minus_one').set_weights([np.array([1.]), np.array([-1.]), np.array([0.]), np.array([1.])])
+    #keras_model.get_layer('met_weight_minus_one').set_weights([np.array([1.,1.]]), np.array([-1.,-1.]), np.array([0.,0.]), np.array([1.,1.])])
 
     return keras_model
 
