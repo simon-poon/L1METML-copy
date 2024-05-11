a1168230e65f9e6e150aa9e2ace5255a0be637b3
simon-poon <93788837+simon-poon@users.noreply.github.com>
2024-05-11 04:12:45 -0700

    Update models.py
    Temp: put back in met_weight_minus_one (to test alongside weighted sum layer)

--- models.py
+++ models.py
@@ -82,7 +82,7 @@ def dense_embedding(n_features=6,
             b = Dense(2, name='met_bias', activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
             pxpy = Add()([pxpy, b])
         w = Dense(1, name='met_weight', activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
-        w = BatchNormalization(trainable=False, name='met_weight_batch_norm', epsilon=False)(w)
+        w = BatchNormalization(trainable=False, name='met_weight_minus_one', epsilon=False)(w)
         x = Multiply()([w, pxpy])
 
         x = weighted_sum_layer(name='output')(x)
@@ -90,7 +90,7 @@ def dense_embedding(n_features=6,
 
     keras_model = Model(inputs=inputs, outputs=outputs)
 
-    #keras_model.get_layer('met_weight_minus_one').set_weights([np.array([1.]), np.array([-1.]), np.array([0.]), np.array([1.])])
+    keras_model.get_layer('met_weight_minus_one').set_weights([np.array([1.]), np.array([-1.]), np.array([0.]), np.array([1.])])
 
     return keras_model
 
@@ -153,7 +153,7 @@ def dense_embedding_quantized(n_features=6,
             b = QDense(2, name='met_bias', kernel_quantizer=logit_quantizer, bias_quantizer=logit_quantizer, kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
             pxpy = Add()([pxpy, b])
         w = QDense(1, name='met_weight', kernel_quantizer=logit_quantizer, bias_quantizer=logit_quantizer, kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
-        w = BatchNormalization(trainable=False, name='met_weight_batch_norm', epsilon=False)(w)
+        w = BatchNormalization(trainable=False, name='met_weight_minus_one', epsilon=False)(w)
         x = Multiply()([w, pxpy])
 
         x = weighted_sum_layer(name='output')(x)
@@ -161,7 +161,7 @@ def dense_embedding_quantized(n_features=6,
 
     keras_model = Model(inputs=inputs, outputs=outputs)
 
-    #keras_model.get_layer('met_weight_minus_one').set_weights([np.array([1.]), np.array([-1.]), np.array([0.]), np.array([1.])])
+    keras_model.get_layer('met_weight_minus_one').set_weights([np.array([1.]), np.array([-1.]), np.array([0.]), np.array([1.])])
 
     return keras_model
 
